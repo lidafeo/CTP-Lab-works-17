3c6e3da860cafc298cf51f85fe71360852b8c780
lidafeo <36775331+lidafeo@users.noreply.github.com>
2018-02-24 21:12:23 +0300

    Update Primes.java

--- zadacha№0/Primes.java
+++ zadacha№0/Primes.java
@@ -1,17 +1,17 @@
-//TODO: Класс Primes
+//Класс Primes
 public class Primes 
 {
-	//TODO: Гланый метод, выводящий простые числа
+	//Гланый метод, выводящий простые числа
 	public static void main(String[] args)
 	{
-		//TODO: Реализация программы
+		//Реализация программы
 		for(int i=2;i<=100;i++)
 			if(isPrime(i)==true)
 			System.out.print(" "+i);
 	}
 	public static boolean isPrime(int n)
 	{
-		//TODO: Проверка числа на простое
+		//Проверка числа на простое
 		for(int i=2;i<n;i=i+1)
 		{
 			if(n%i==0)
